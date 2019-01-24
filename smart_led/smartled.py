6a1dbbb49e984c25c7ec8869404c05d288607d55
Clement Dessoude <clement.dessoude@gmail.com>
2019-01-24 09:45:00 +0100

    update smart_led

--- smart_led/smartled.py
+++ smart_led/smartled.py
@@ -1,4 +1,5 @@
 from flask import Flask, json, request
+import requests
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ def state():
             mimetype='appliaction/json'
         )
         return response
-    
+
     response = app.response_class(
         response=json.dumps(data),
         status=200,
@@ -32,18 +33,16 @@ def state():
 def on():
     data = request.get_json(force=True)
     print(data)
-    state = {"R": 0, "G": 0, "B": 0}
-    with open("state.json") as file:
+    state = {'r': 0, 'g': 0, 'b': 0, 'a': 0}
+    with open("state.json", "r") as file:
         state = json.loads(file.read())
-        R,G,B = data["payload"].split('-')
-        state["R"] = int(R)
-        state["G"] = int(G)
-        state["B"] = int(B)
+        state = data["payload"]
     # change actual led colors here
     print(state)
     with open("state.json", "w") as file:
         file.write(json.dumps(state))
-    
+
+    send_status()
     response = app.response_class(
         status=200
     )
@@ -52,9 +51,11 @@ def on():
 
 @app.route("/off", methods=["POST"])
 def off():
-    state = {"R": 0, "G": 0, "B": 0}
-    with open("state.json", "r") as file:
+    state = {'r': 0, 'g': 0, 'b': 0, 'a': 0}
+    with open("state.json", "w") as file:
         file.write(json.dumps(state))
+
+    send_status()
     # turn off leds here
     response = app.response_class(
         status=200
@@ -74,7 +75,7 @@ def getConfigResponse():
             mimetype='application/json'
         )
         return response
-    
+
     response = app.response_class(
         response=json.dumps(data),
         status=500,
@@ -82,6 +83,44 @@ def getConfigResponse():
     )
     return response
 
+def send_status():
+    data = {}
+    with open("state.json", "r") as file:
+        state = json.loads(file.read())
+        data["value"] = json.dumps(state)
+
+    with open("server.json", "r") as file:
+        server = json.loads(file.read())
+        data_source_id = server["data-source-ids"]["state"]
+        data["data_source_id"] = data_source_id
+
+    r = requests.post("http://" + server["url"] + ":" + server["port"] + "/saveDataPoint", data=json.dumps(data),
+        headers={'Content-type': 'application/json'})
+
+    print("response datasaved", r.text, data)
+
+    return app.response_class(status=200)
+
+@app.route('/serverConfig', methods=['POST'])
+def setServerConfig():
+    try:
+        data = request.get_json(force=True)
+        server_config = {}
+        server_config["url"] = data.get("url")
+        server_config["id"] = data.get("id")
+        server_config["port"] = data.get("port")
+        server_config["data-source-ids"] = data.get("data-source-ids")
+    except:
+        return app.response_class(status=400)
+
+
+    with open("server.json", "w") as file:
+        file.write(json.dumps(server_config))
+
+    response = app.response_class(
+        status=200
+    )
+    return response
 
 if __name__=='__main__':
     app.run()
